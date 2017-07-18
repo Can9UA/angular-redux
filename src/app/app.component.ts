c4e83488c0969596f70066581a67bda85e9a01c9
alexandr.poddubnyi <can9.onet@gmail.com>
2017-07-18 13:02:31 +0300

    added date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ import 'rxjs/add/observable/interval';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  clock = Observable.interval(1000);
+  clock = Observable.interval(1000)
+                    .map(() => new Date());
 
   constructor() {
     this.clock.subscribe((i) => console.log(i));
