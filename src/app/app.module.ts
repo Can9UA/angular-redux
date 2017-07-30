696a84b5e89fc7ac4fc407b352094738f58722d5
alexandr.poddubnyi <can9.onet@gmail.com>
2017-07-30 12:46:47 +0300

    clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PersonsTimeModule } from './persons-time/persons-time.module';
-import { PeopleFilterModule } from './people-filter/people-filter.module';
-
 import { StoreModule } from '@ngrx/store';
 
-import { clock, people } from './persons-time/state-management/reducers';
-
-import { people_f } from './people-filter/reducers/people_f';
-import { filter } from './people-filter/reducers/filter';
 
 @NgModule({
   declarations: [
@@ -18,16 +11,7 @@ import { filter } from './people-filter/reducers/filter';
   ],
   imports: [
     BrowserModule,
-    PersonsTimeModule,
-    PeopleFilterModule,
-    StoreModule.provideStore({ // TODO how to add new store values if it already exist via provideStore
-      // example 1
-      people,
-      clock,
-      //example 2
-      people_f,
-      filter
-    })
+    // StoreModule.provideStore({})
   ],
   providers: [],
   bootstrap: [AppComponent]
